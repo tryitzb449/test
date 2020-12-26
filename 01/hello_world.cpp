58ac93ba9f9574a1705dc5492cd5b2390a1a6fd3
robert <592007019@qq.com>
2020-12-27 06:08:29 +0800

    添加Hello,World程序

---
+++ 01/hello_world.cpp
@@ -0,0 +1,7 @@
+#include "../std_lib_facilities.h"
+int main()      //C++从main函数开始执行
+{
+    cout<<"Hello,World!\n";     //输出“Hello,World!”
+    keep_window_open();         //等待输入一个字符
+    return 0;
+}
