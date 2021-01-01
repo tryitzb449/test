f2747f7a2f466dcab064e8a0fc642818bef67c92
robert <592007019@qq.com>
2021-01-01 15:53:47 +0800

    添加第二章习题

---
+++ 02/demo-2-1.cpp
@@ -0,0 +1,8 @@
+#include "../std_lib_facilities.h"
+int main()      //C++从main函数开始执行
+{
+    cout<<"Hello,programming!\n";     //输出“Hello,World!”
+    cout<<"Here we go!\n";
+    keep_window_open();         //等待输入一个字符
+    return 0;
+}
